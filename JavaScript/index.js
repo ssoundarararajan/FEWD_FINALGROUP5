bcaf742b34b5632394c6c5ed8df5d76ebb886c19
Sairam Soundararajan <sai20879@gmail.com>
2024-05-06 20:56:08 -0400

    updates

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -2,6 +2,7 @@ document.getElementById("showLinksBtn").addEventListener("click", function() {
     var linksList = document.getElementById("linksList");
     if (linksList.style.display === "none") {
       linksList.style.display = "block";
+      linksList.style.textAlign = "center";
       linksList.innerHTML = "<li><a href='http://www.ubuntu.com'>Ubuntu Linux</a></li>" +
                             "<li><a href='http://www.linuxmint.com'>Linux Mint</a></li>" +
                             "<li><a href='http://www.zorin.com'>Zorin OS</a></li>" +
